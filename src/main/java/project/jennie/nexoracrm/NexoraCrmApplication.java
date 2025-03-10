74a3d16af7d89bd135e2be19231af59a0692c405
ChoiJiHye <stdy1227@gmail.com>
2025-03-10 11:15:32 +0900

    환경설정 1 - 연결 확인

---
+++ src/main/java/project/jennie/nexoracrm/NexoraCrmApplication.java
@@ -0,0 +1,13 @@
+package project.jennie.nexoracrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexoraCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/jennie/nexoracrm/NexoraCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package project.jennie.nexoracrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
